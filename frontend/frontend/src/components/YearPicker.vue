4fc4c9968704af1acbfe3cb2d01d3721120bda40
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-07-08 00:12:05 +0200

    Add YearPicker component

---
+++ frontend/frontend/src/components/YearPicker.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row items-center justify-center q-my-sm">
+        <q-btn dense round @click="() => { year--; $emit('input', year) }" icon="chevron_left" />
+        <div color="primary" class="q-mx-md text-center">{{year}}</div>
+        <q-btn dense round @click="() => { year++; $emit('input', year) }" icon="chevron_right" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    props: {
+        'value': Number,
+        'show': Boolean
+    },
+    setup(props) {
+        const year = ref<number>(props.value)
+        return { year }
+    },
+})
+</script>
